746fc8f2179e368465453e52812ee92860e40783
rid137 <makinderidwan73@gmail.com>
2024-12-24 09:04:13 +0100

    npm version updated

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,6 +1,6 @@
 export const truncate = (str: string, wordAmout: number) => {
     if(str?.length > wordAmout) {
         str = str.substring(0, wordAmout) + '...'
-    }
+    };
     return str;
   };
